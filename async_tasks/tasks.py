21511a7a62aa19ada85e6216c529e08d35d1b1b9
mabeijing <58149278@qq.com>
2021-02-03 02:45:12 +0800

    celery异步执行，进度条功能

--- async_tasks/tasks.py
+++ async_tasks/tasks.py
@@ -1,10 +1,23 @@
 import time
 from async_tasks import ext
+
 app = ext.celery
 
 
 # 异步任务
-@app.task
-def send_template_sms():
-    time.sleep(10)
-    return 'success'
+@app.task(bind=True)
+def send_template_sms(self, x, y):
+    """接受参数，支持self.request"""
+    time.sleep(5)
+    print(self.request)
+    return x + y
+
+
+@app.task(bind=True)
+def socket_bar(self):
+    """编写异步任务时。可以在适当位置增加update_state。让整个程序执行结束打印信息"""
+    for i in range(1, 11):
+        time.sleep(1)
+        self.update_state(state="PROGRESS", meta={'p': i * 10})
+    print(self.request)
+    return 'finish'
--- views/tasks.py
+++ views/tasks.py
@@ -1,11 +1,40 @@
 # -*- coding:utf-8 -*-
-from flask import Blueprint, g
-from async_tasks.tasks import send_template_sms
+from flask import Blueprint
+from celery.result import AsyncResult
+from async_tasks.tasks import send_template_sms, socket_bar
 
 task = Blueprint('task', __name__, url_prefix='/task')
 
 
+# 执行异步任务，返回任务id
 @task.route('/execute')
-def good_lists():
-    send_template_sms.delay()
-    return 'execute success'
+def execute_task():
+    task_result = send_template_sms.delay(4, 5)
+    task_id = task_result.id
+    return task_id
+
+
+# 增加执行结果查询
+@task.route('/query/<tid>')
+def query_task(tid):
+    res = AsyncResult(tid)
+    print(res.result)
+    return 'res.result'
+
+
+# 带进度条的显示
+@task.route('/finish_bar')
+def finish_bar():
+    def bar(body):
+        res = body.get('result')
+        if body.get('status') == 'PROGRESS':
+            print('\r任务进度: {0}%'.format(res.get('p')))
+            # 注释方法可以在单挑记录刷新任务进度，print打印多条记录
+            # sys.stdout.write('\r任务进度: {0}%'.format(res.get('p')))
+            # sys.stdout.flush()
+        else:
+            print('\r')
+            print(res)
+
+    socket_bar.delay().get(on_message=bar, propagate=False)
+    return 'finish'
