196bcbbaebef6c8de36a3ed1dc6bd5363f7cf1ee
mabeijing <58149278@qq.com>
2021-02-03 01:01:18 +0800

    补充celery说明

--- async_tasks/__init__.py
+++ async_tasks/__init__.py
@@ -7,6 +7,7 @@ Flask	1.1.2
 1、在__init__下创建ext。导入到app工厂下ext.init_app(app)实例化
 2、将tasks下的ext导入到真正的任务模块，celery = ext.celery
 3、当前__init__.py只能生成ext提供工厂函数实例化。然后才能在tasks下ext.celery
+4、并且，在工厂函数下，必须在陆游注册前，实例化ext
 
 4、真正的celery，需要分布式执行。
 5、创建一个和async_tasks同结构的文件夹
